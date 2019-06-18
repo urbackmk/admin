1633e6aa2cc66d92e4bd8d03e3ccef8f00fb11f6
meganrm <meganrm@gmail.com>
2019-06-18 16:41:51 -0700

    small fixes and adding error logging

--- src/containers/SubscriberSignup/index.js
+++ src/containers/SubscriberSignup/index.js
@@ -51,7 +51,7 @@ class SubscriberSignup extends React.Component {
     }
 
     componentDidMount() {
-        const {getAllSubscribers} = this.props;
+        const { getAllSubscribers } = this.props;
         getAllSubscribers();
     }
 
