38d1a04c7a030ff5864a3c0daee79af234f18211
meganrm <meganrm@gmail.com>
2019-07-21 18:40:37 -0700

    filtered down to replies

--- src/state/sms-users/selectors.js
+++ src/state/sms-users/selectors.js
@@ -5,5 +5,9 @@ export const getTotalSMSUsers = state => state.smsUsers.totalSmsUsers;
 export const getUserCache = state => state.smsUsers.userCache;
 
 export const getUsersWithMessages = createSelector([getUserCache], (users) => {
-    return filter(users, (ele) => ele.messages && ele.messages.length)
+    return filter(users, (ele) => ele.messages && ele.messages.length);
+})
+
+export const getUsersWithReplies = createSelector([getUsersWithMessages], (users) => {
+  return filter(users, (ele) => ele.messages.length > 1);
 })
