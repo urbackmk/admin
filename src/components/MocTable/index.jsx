58f163bdb2a661f0ddb6de6e586d20d0e83e9776
TheMiloAnderson <themiloanderson@gmail.com>
2019-07-28 00:40:45 -0700

    Search MoC table by displayName and/or state

--- src/components/MocTable/index.jsx
+++ src/components/MocTable/index.jsx
@@ -4,9 +4,16 @@ import {
     Table,
     Checkbox,
     Input,
+    Button,
+    Icon,
 } from 'antd';
 
 export default class MocTable extends React.Component {
+
+  state = {
+    searchText: '',
+  }
+
   updateMissingMember({target}, id) {
     const { updateMissingMemberValue } = this.props;
     updateMissingMemberValue(id, target.checked);
@@ -21,6 +28,58 @@ export default class MocTable extends React.Component {
     const { updateDisplayNameValue } = this.props;
     updateDisplayNameValue(id, target.value);
   }
+
+  getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={node => {
+            this.searchInput = node;
+          }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => this.handleSearch(selectedKeys, confirm)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Button
+          type="primary"
+          onClick={() => this.handleSearch(selectedKeys, confirm)}
+          icon="search"
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          Search
+        </Button>
+        <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+          Reset
+        </Button>
+      </div>
+    ),
+    filterIcon: filtered => (
+      <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: visible => {
+      if (visible) {
+        setTimeout(() => this.searchInput.select());
+      }
+    },
+  });
+
+  handleSearch = (selectedKeys, confirm) => {
+    confirm();
+    this.setState({ searchText: selectedKeys[0] });
+  };
+
+  handleReset = (clearFilters) => {
+    clearFilters();
+    this.setState({ searchText: '' });
+  };
   
   render() {
     const { mocs } = this.props;
@@ -34,11 +93,13 @@ export default class MocTable extends React.Component {
             onChange={(e) => this.updateDisplayName(e, record.govtrack_id)}
           />
         ),
+        ...this.getColumnSearchProps('displayName'),
       },
       {
         title: 'State',
         dataIndex: 'state',
         key: 'state',
+        ...this.getColumnSearchProps('state'),
       },
       {
         title: 'Chamber',
