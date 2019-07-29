4e1819cf13784dfcff58046d763f57a4b5a4393c
TheMiloAnderson <themiloanderson@gmail.com>
2019-07-28 23:11:15 -0700

    MoC details modal window

--- src/components/MocTable/index.jsx
+++ src/components/MocTable/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-
+import './style.scss';
 import {
     Table,
     Checkbox,
     Input,
     Button,
     Icon,
+    Modal,
 } from 'antd';
 
 export default class MocTable extends React.Component {
 
   state = {
     searchText: '',
+    modalVisible: false,
+    modalRecord: {},
   }
 
   updateMissingMember({target}, id) {
@@ -48,11 +51,13 @@ export default class MocTable extends React.Component {
           icon="search"
           size="small"
           style={{ width: 90, marginRight: 8 }}
-        >
-          Search
+        >Search
         </Button>
-        <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
-          Reset
+        <Button 
+          onClick={() => this.handleReset(clearFilters)}
+          size="small"
+          style={{ width: 90 }}
+        >Reset
         </Button>
       </div>
     ),
@@ -80,6 +85,18 @@ export default class MocTable extends React.Component {
     clearFilters();
     this.setState({ searchText: '' });
   };
+
+  viewRecord = (record) => {
+    console.log(record);
+    this.setState({ 
+      modalVisible: true,
+      modalRecord: record,
+    });
+  };
+
+  handleModalCancel = () => {
+    this.setState({ modalVisible: false });
+  }
   
   render() {
     const { mocs } = this.props;
@@ -129,25 +146,158 @@ export default class MocTable extends React.Component {
       },
       {
         title: 'Missing member',
-        key: 'action',
+        key: 'missing_member',
         render: (mocs, record) => (
           <span>
             <Checkbox 
               key={record.govtrack_id}
               onChange={(e) => this.updateMissingMember(e, record.govtrack_id)} 
               defaultChecked={record.missing_member && record.missing_member[116]}>
-                  Missing Member
+                  Missing
             </Checkbox>
           </span>
         ),
       },
+      {
+        title: 'Actions',
+        dataIndex: '',
+        key: 'actions',
+        render: (mocs, record) => (
+          <span>
+            <Button 
+              onClick={() => this.viewRecord(record)}
+              size="small"
+            >Details
+            </Button>
+          </span>
+        ),
+      }
     ];
     return (
-      <Table 
-        columns={columns}
-        dataSource={mocs}
-        rowKey={'govtrack_id'}
-      />
+      <div>
+        <Table 
+          columns={columns}
+          dataSource={mocs}
+          rowKey={'govtrack_id'}
+        />
+        <Modal
+          title={this.state.modalRecord.displayName + ' | ' + this.state.modalRecord.title}
+          visible={this.state.modalVisible}
+          onOk={this.handleModalUpdate}
+          onCancel={this.handleModalCancel}
+          width={800}
+        >
+          <h2>Personal Details</h2>
+          <table>
+            <tr>
+              <th>Chamber</th>
+              <td>{this.state.modalRecord.chamber}</td>
+            </tr>
+            <tr>
+              <th>Date of Birth</th>
+              <td>{this.state.modalRecord.date_of_birth}</td>
+            </tr>
+            <tr>
+              <th>District</th>
+              <td>{this.state.modalRecord.district}</td>
+            </tr>
+            <tr>
+              <th>Next Election</th>
+              <td>{this.state.modalRecord.next_election}</td>
+            </tr>
+            <tr>
+              <th>Party</th>
+              <td>{this.state.modalRecord.party}</td>
+            </tr>
+            <tr>
+              <th>Gender</th>
+              <td>{this.state.modalRecord.gender}</td>
+            </tr>
+            <tr>
+              <th>In Office</th>
+              <td>{this.state.modalRecord.in_office ? 'Yes' : 'No'}</td>
+            </tr>
+            <tr>
+              <th>Missing Member</th>
+              <td>{this.state.modalRecord.missing_member ? 
+                this.state.modalRecord.missing_member[116] ? 'Yes' : 'No' 
+                  : ''}</td>
+            </tr>
+            <tr>
+              <th>Votes With Party</th>
+              <td>{this.state.modalRecord.votes_with_party_pct}%</td>
+            </tr>
+            <tr>
+              <th>Missed Votes</th>
+              <td>{this.state.modalRecord.missed_votes_pct}%</td>
+            </tr>
+            <tr>
+              <th>FEC Candidate ID</th>
+              <td>{this.state.modalRecord.fec_candidate_id}</td>
+            </tr>
+            <tr>
+              <th>Senate Class</th>
+              <td>{this.state.modalRecord.senate_class}</td>
+            </tr>
+            <tr>
+              <th>Seniority</th>
+              <td>{this.state.modalRecord.seniority}</td>
+            </tr>
+            <tr>
+              <th>State</th>
+              <td>{this.state.modalRecord.state}</td>
+            </tr>
+            <tr>
+              <th>State Rank</th>
+              <td>{this.state.modalRecord.state_rank}</td>
+            </tr>
+          </table>
+
+          <h2>Contact Info</h2>
+          <table>
+            <tr>
+              <th>Phone</th>
+              <td>{this.state.modalRecord.phone}</td>
+            </tr>
+            <tr>
+              <th>Fax</th>
+              <td>{this.state.modalRecord.fax}</td>
+            </tr>
+            <tr>
+              <th>Office</th>
+              <td>{this.state.modalRecord.office}</td>
+            </tr>
+            <tr>
+              <th>Address</th>
+              <td>{this.state.modalRecord.address}</td>
+            </tr>
+          </table>
+
+          <h2>Media</h2>
+          <table>
+            <tr>
+              <th>Facebook Account</th>
+              <td>{this.state.modalRecord.facebook_account}</td>
+            </tr>
+            <tr>
+              <th>RSS URL</th>
+              <td><a href={this.state.modalRecord.rss_url}>{this.state.modalRecord.rss_url}</a></td>
+            </tr>
+            <tr>
+              <th>Twitter Account</th>
+              <td>{this.state.modalRecord.twitter_account}</td>
+            </tr>
+            <tr>
+              <th>Website</th>
+              <td><a href={this.state.modalRecord.url}>{this.state.modalRecord.url}</a></td>
+            </tr>
+            <tr>
+              <th>YouTube Account</th>
+              <td>{this.state.modalRecord.youtube_account}</td>
+            </tr>
+          </table>
+        </Modal>
+      </div>
     )
   }
 }
