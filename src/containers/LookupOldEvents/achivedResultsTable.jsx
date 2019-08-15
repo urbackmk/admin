203c22207e7e7663e6c7dc82c0be52743cc48e63
Meghan Urback <meghan@textio.com>
2019-08-15 13:40:55 -0700

    dumb way of adding icon flag images but that's ok

--- src/containers/LookupOldEvents/achivedResultsTable.jsx
+++ src/containers/LookupOldEvents/achivedResultsTable.jsx
@@ -4,6 +4,26 @@ import {
     Table,
 } from 'antd';
 
+import activism from '../../assets/img/icon-flags/activism.svg';
+import campaign from '../../assets/img/icon-flags/campaign.svg';
+import hrOne from '../../assets/img/icon-flags/hr-one.svg';
+import inPerson from '../../assets/img/icon-flags/in-person.svg';
+import mfol from '../../assets/img/icon-flags/mfol.svg';
+import nextGen from '../../assets/img/icon-flags/next-gen.svg';
+import phoneIn from '../../assets/img/icon-flags/phone-in.svg';
+import staff from '../../assets/img/icon-flags/staff.svg';
+
+const iconFlagMap = {
+    activism: activism,
+    campaign: campaign,
+    'hr-one': hrOne,
+    'in-person': inPerson,
+    mfol: mfol,
+    'next-gen': nextGen,
+    'phone-in': phoneIn,
+    staff: staff,
+};
+
 const ResultsTable = ({ allOldEvents }) => {
     const columns = [
         {
@@ -18,7 +38,13 @@ const ResultsTable = ({ allOldEvents }) => {
         },
         {
             title: 'Icon',
-            dataIndex: 'iconFlag',
+            render: (text, record) => <img
+                src={iconFlagMap[record.iconFlag]}
+                // not sure why the below line isn't working
+                // src={`../../assets/img/icon-flags/${record.iconFlag}.svg`}
+                alt={record.iconFlag}
+                width={30} 
+            />,
             key: 'iconFlag',
         },
         {
